e19f9019e5cd496a6199cf525d1d58c97e306f4c
Shaine Jimenez <jimenez.shaine1@gmail.com>
2023-03-09 09:11:37 -0600

    adding table exercises

---
+++ db_tables_exercises.sql
@@ -0,0 +1,31 @@
+show databases; 
+use albums_db;
+select database();
+show tables;
+use employees;
+select database();
+show tables;
+-- Which table(s) do you think contain a numeric type column?
+-- department,dept_emp, employees, salaries, titles 
+-- Which table(s) do you think contain a string type column? 
+-- departments, dept_emp, dept_manager, employees, titles
+-- Which table(s) do you think contain a date type column? 
+-- dept_emp, dept_manager, employees, salaries, titles
+
+show create table dept_manager;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
